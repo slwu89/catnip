755d2a84f5e6f192566f0b6e7c9d687a1f36beee
slwu89 <slwu89@users.noreply.github.com>
2023-09-25 21:27:50 -0700

    alternate syntax for delta migration

--- datamigration.jl
+++ datamigration.jl
@@ -15,6 +15,8 @@ end
 # pretty graphviz settings
 to_graphviz(SchDDS,graph_attrs=Dict("dpi"=>"72","size"=>"4","ratio"=>"expand"))
 
+# ----------------------------------------------------------------------
+# delta (pullback) migration
 dds = @acset DDS begin
   X=7
   Φ=[4,4,5,5,5,7,6]
@@ -29,5 +31,36 @@ F = @migration SchGraph SchDDS begin
 end
 
 g = migrate(Graph, dds, F)
-
 to_graphviz(g,node_labels=true,edge_labels=true)
+
+# a different F that now gives the graph of what state it came from
+F = @migration SchGraph SchDDS begin
+  V => X
+  E => X
+  src => Φ
+  tgt => id(X)
+end
+
+g = migrate(Graph, dds, F)
+to_graphviz(g,node_labels=true,edge_labels=true)
+
+# alternate specification syntax
+F = @finfunctor SchGraph SchDDS begin
+  V => X
+  E => X
+  src => id(X)
+  tgt => Φ
+end
+
+Δ = DataMigrationFunctor(F, DDS, Graph)
+Δ(dds)
+
+# ----------------------------------------------------------------------
+# ex from Functorial Data Migration paper
+
+@present C(FreeSchema) begin
+  
+end
+
+# ----------------------------------------------------------------------
+# general conjunctive migrations
